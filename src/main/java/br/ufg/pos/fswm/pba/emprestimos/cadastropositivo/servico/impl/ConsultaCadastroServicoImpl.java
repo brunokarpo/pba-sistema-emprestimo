3cf2a5504ba1135ae15ba958c667fed012ec1805
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2017-10-19 20:58:42 -0200

    Adicionando anotação para determinar o bean primário.

--- src/main/java/br/ufg/pos/fswm/pba/emprestimos/cadastropositivo/servico/impl/ConsultaCadastroServicoImpl.java
+++ src/main/java/br/ufg/pos/fswm/pba/emprestimos/cadastropositivo/servico/impl/ConsultaCadastroServicoImpl.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,7 @@ import java.util.Arrays;
  * @date 19/10/17.
  */
 @Service
+@Primary
 public class ConsultaCadastroServicoImpl implements ConsultaCadastroServico {
 
     private static final Logger LOG = LoggerFactory.getLogger(ConsultaCadastroServicoImpl.class);
