ff4aa5dc6a3ac7afc5f48bda4e6a1c97ffd83aa4
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2017-10-19 20:50:29 -0200

    Refatorando código de Risco.

--- src/main/java/br/ufg/pos/fswm/pba/emprestimos/cadastropositivo/modelo/Risco.java
+++ src/main/java/br/ufg/pos/fswm/pba/emprestimos/cadastropositivo/modelo/Risco.java
@@ -1,14 +1,22 @@
 package br.ufg.pos.fswm.pba.emprestimos.cadastropositivo.modelo;
 
+import java.util.Arrays;
+
 /**
  * @author Bruno Nogueira de Oliveira
  * @date 19/10/17.
  */
 public enum Risco {
-    BAIXO,
-    MEDIO,
-    ALTO,
-    NEGATIVADO;
+    BAIXO(Constants.NIVEL_1, Constants.NIVEL_2),
+    MEDIO(Constants.NIVEL_0, Constants.NIVEL_3),
+    ALTO(Constants.NIVEL_4),
+    NEGATIVADO(Constants.NIVEL_5);
+
+    private final Integer[] niveis;
+
+    Risco(Integer... niveis) {
+        this.niveis =  niveis;
+    }
 
     /**
      * Calcula o nível de Risco do cliente de acordo com o nível recebido pelo Sistema de Consulta Positivo.
@@ -17,18 +25,20 @@ public enum Risco {
      * @return instância de {@link Risco} de acordo com o cliente;
      */
     public static Risco calculaRisco(Integer nivel) {
-        switch(nivel.intValue()) {
-            case 0:
-            case 3:
-                return Risco.MEDIO;
-            case 1:
-            case 2:
-                return Risco.BAIXO;
-            case 5:
-                return Risco.NEGATIVADO;
-            case 4:
-            default:
-                return Risco.ALTO;
+        for (Risco risco : Risco.values()) {
+            if(Arrays.asList(risco.niveis).contains(nivel)) {
+                return risco;
+            }
         }
+        return Risco.ALTO;
+    }
+
+    private static class Constants {
+        static final int NIVEL_1 = 1;
+        static final int NIVEL_2 = 2;
+        static final int NIVEL_0 = 0;
+        static final int NIVEL_3 = 3;
+        static final int NIVEL_4 = 4;
+        static final int NIVEL_5 = 5;
     }
 }
