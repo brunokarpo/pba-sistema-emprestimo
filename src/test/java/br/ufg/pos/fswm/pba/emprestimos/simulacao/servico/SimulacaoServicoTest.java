963120becc0d8e9c2658f2631396ace62dd0220b
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2017-10-29 13:04:23 -0200

    Criando pessoa para ser devolvida ao consultar um contrato.

--- src/test/java/br/ufg/pos/fswm/pba/emprestimos/simulacao/servico/SimulacaoServicoTest.java
+++ src/test/java/br/ufg/pos/fswm/pba/emprestimos/simulacao/servico/SimulacaoServicoTest.java
@@ -1,5 +1,8 @@
 package br.ufg.pos.fswm.pba.emprestimos.simulacao.servico;
 
+import br.ufg.pos.fswm.pba.emprestimos.cadastropositivo.modelo.Risco;
+import br.ufg.pos.fswm.pba.emprestimos.cliente.modelo.Pessoa;
+import br.ufg.pos.fswm.pba.emprestimos.cliente.modelo.Sexo;
 import br.ufg.pos.fswm.pba.emprestimos.cliente.servico.PessoaServico;
 import br.ufg.pos.fswm.pba.emprestimos.cliente.servico.exceptions.CpfNaoEncontradoException;
 import br.ufg.pos.fswm.pba.emprestimos.simulacao.servico.impl.SimulacaoServicoImpl;
@@ -11,6 +14,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Month;
+
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -21,7 +28,13 @@ import static org.mockito.Mockito.when;
 @RunWith(SpringRunner.class)
 public class SimulacaoServicoTest {
 
-    public static final String CPF = "89900534042";
+    private static final String NOME = "Milena Sophia Gomes";
+    private static final String CPF = "37270463689";
+    private static final String PROFISSAO = "Programador de Software";
+    private static final BigDecimal SALARIO = new BigDecimal(3000.00);
+    private static final LocalDate NASCIMENTO = LocalDate.of(1995, Month.FEBRUARY, 22);
+    private static final Sexo SEXO = Sexo.FEMININO;
+    public static final Risco RISCO = Risco.ALTO;
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
@@ -29,11 +42,24 @@ public class SimulacaoServicoTest {
     @MockBean
     private PessoaServico pessoaServicoMock;
 
+    private Pessoa pessoa;
+
     private SimulacaoServico sut;
 
     @Before
     public void setUp() throws Exception {
         sut = new SimulacaoServicoImpl(pessoaServicoMock);
+
+        pessoa = new Pessoa();
+        pessoa.setNome(NOME);
+        pessoa.setCpf(CPF);
+        pessoa.setNascimento(NASCIMENTO);
+        pessoa.setProfissao(PROFISSAO);
+        pessoa.setSalario(SALARIO);
+        pessoa.setSexo(SEXO);
+        pessoa.setRisco(RISCO);
+
+        when(pessoaServicoMock.buscarPorCpf(CPF)).thenReturn(pessoa);
     }
 
     @Test
